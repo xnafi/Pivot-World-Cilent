0a1e00bbf2569a54fbc434819ef3b1cabf4481d3
Forhad Khan <fk.nafi11@gmail.com>
2022-10-27 00:19:54 +0600

    readme file create

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link, useLoaderData } from 'react-router-dom'
+import Pdf from "react-to-pdf";
 
 const CourseDetails = () => {
+  const ref = React.createRef();
   const course = useLoaderData()
   const { image, des, name, courseDetail, duration } = course
 
   return (
     <div className="bg-white dark:bg-gray-900 h-full md:h-full w-full px-8 bgCourse bg-center bg-fixed bg-cover bg-no-repeat flex flex-col md:flex-row justify-center items-center" >
-      <div className="md:w-3/4 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50 bg-slate-700 my-10 mt-28">
+
+      <div className="md:w-3/4 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50 bg-slate-700 my-10 mt-28" ref={ref}>
         <img src={image} alt="" className="object-fill object-center w-full h-96 rounded-md dark:bg-gray-500" />
         <div className="mt-6 mb-2 text-white px-4 space-y-2">
           <h2 className="text-xl font-semibold tracking-wide">{name}</h2>
@@ -20,7 +23,15 @@ const CourseDetails = () => {
             courseDetail.map(data => <li>{data}</li>)
           }
         </ul>
-        <Link to='/courses' className="btn btn-outline btn-warning my-4 ml-3 px-10">Courses</Link>
+        <div className='flex justify-between items-center px-3'>
+          <Pdf className='bg-slate-500' targetRef={ref} filename="code-example.pdf">
+            {({ toPdf }) =>
+              <button className="btn btn-outline btn-warning my-4 px-10" onClick={toPdf}>Download</button>}
+          </Pdf>
+
+          <Link to='/courses' className="btn btn-outline btn-warning my-4 px-10">Courses</Link>
+        </div>
+
       </div>
     </div>
   )
