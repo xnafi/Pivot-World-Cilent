7d73458ab6a36257c1103fc8e3c4d15333a81998
Forhad Khan <fk.nafi11@gmail.com>
2022-10-27 18:04:03 +0600

    fix button details page

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -10,7 +10,7 @@ const CourseDetails = () => {
   return (
     <div className="bg-white dark:bg-gray-900 h-full md:h-full w-full px-8 bgCourse bg-center bg-fixed bg-cover bg-no-repeat flex flex-col md:flex-row justify-center items-center" >
 
-      <div className="md:w-3/4 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50 bg-slate-700 my-10 mt-28" ref={ref}>
+      <div className="md:w-3/4 w-screen px-2 md:px-0 rounded-md shadow-md dark:bg-gray-900 dark:text-gray-50 bg-slate-700 my-10 mt-28" ref={ref}>
         <img src={image} alt="" className="object-fill object-center w-full h-96 rounded-md dark:bg-gray-500" />
         <div className="mt-6 mb-2 text-white px-4 space-y-2">
           <h2 className="text-xl font-semibold tracking-wide">{name}</h2>
@@ -23,12 +23,12 @@ const CourseDetails = () => {
             courseDetail.map(data => <li>{data}</li>)
           }
         </ul>
-        <div className='flex justify-between items-center px-3'>
+        <div className='flex justify-between flex-col md:flex-row items-center px-4'>
           <Pdf className='bg-slate-500' targetRef={ref} filename="code-example.pdf">
             {({ toPdf }) =>
-              <button className="btn btn-outline btn-warning my-4 px-10" onClick={toPdf}>Download</button>}
+              <button className="btn btn-outline btn-warning my-2" onClick={toPdf}>Download</button>}
           </Pdf>
-          <Link to={`/purchase/${id}`} className="btn btn-outline btn-info text-md">Get premium access</Link>
+          <Link to={`/purchase/${id}`} className="btn btn-outline btn-info text-sm my-2">Get premium access</Link>
 
           {/* <Link to='/courses' className="btn btn-outline btn-warning my-4 px-10">Courses</Link> */}
         </div>
